68932a67e7491f099e727928464597975ebfb5db
lomo <cokeneon@sks1996.com>
2024-10-14 11:47:36 +0900

    first commit in office

---
+++ 1168.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// This code is counting ages.
+class Main {
+    public static void main(String[] args) {
+        int age;
+        int[] list = new int[2];
+        Scanner input = new Scanner(System.in);
+        
+        for (int i=0;i<2;i++) {
+            list[i] = input.nextInt();
+        }
+        input.close();
+
+        if(list[1]>=3){
+            age = 13-(list[0]/10000);
+        }
+        else{
+            age = 113-(list[0]/10000);
+        }
+        System.out.println(age);
+    }
+}

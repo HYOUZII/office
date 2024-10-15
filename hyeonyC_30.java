303a7093a7b4631f6269329b89722a1473f7c5f1
lomo <cokeneon@sks1996.com>
2024-10-15 14:03:32 +0900

    241015 java 코드 실습

---
+++ hyeonyC_30.java
@@ -0,0 +1,26 @@
+// import java.util.*;
+
+// class Main {
+//     public static void main(String[] args){
+//         System.out.println("Buffer에 대해서 배우자, 창고에서 물자를 나를 때 쓰는 수레라고 생각해라");
+
+//         Scanner input = new Scanner(System.in);
+        
+//         System.out.println("nextLine 명령은 정수 뒤에 붙은 \\n을 보고 문자열을 입력하지 않고 엔터를 쳤다고 생각함 ");
+//         // System.out.println("정수입력 : ");
+//         // int n = input.nextInt();
+
+//         // System.out.println("문자열 입력 : ");
+//         // String str = input.nextLine();
+//         System.out.println("이번에는 진짜 정수입력 : ");
+//         int n = input.nextInt(); // "13\n"를 입력했다면 13을 가져오고 '  \n'이 남는다.
+//         input.nextLine(); // 입력 버퍼에 남은 엔터를 가져옴
+
+//         System.out.println("진짜 문자열 입력 : ");
+//         String str = input.nextLine();
+//         input.close();
+
+//         System.out.println("입력된 수는 : "+ n +" 그리고 문자열 :"+ str);
+
+//     }
+// }
---
+++ hyeonyC_31.java
@@ -0,0 +1,15 @@
+// class Main {
+//     public static void main(String[] args) {
+//         // double num = Math.random(); // 0.0 ~ 0.99999999 사이의 실수
+//         int begine, end;
+//         begine = 50;
+//         end = 60 - begine + 1;
+
+//         for (int i = 0; i < 20; i++) {
+//             double num = (int) (Math.random() * end) + begine; // 랜덤값에 101을 곱하면 정수부가 101을 넘지 못한다.
+//             // (0 ~ 100 사이 정수를 랜덤으로 구할 수 있음)
+//             System.out.print(num + "\t");
+//         }
+
+//     }
+// }

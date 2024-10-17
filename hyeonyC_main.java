e17f025ec0342bd569210aa0efedf013af6c2ff2
lomo <cokeneon@sks1996.com>
2024-10-17 17:45:08 +0900

    지금까지 파일들 정리+강의 41

--- hyeonyC_main.java
+++ hyeonyC_main.java
@@ -1,23 +1,23 @@
-//import java.util.*;
+import java.util.*;
+//[Class 41] 메소드의 매개변수에 대해서 배우자.
 class Main{
 
-    public static void printstar(){//[Class 40] 메소드 기초
-        for(int i=0;i<3;i++){
-        System.out.println("*****");
-        }
+    public static void getsum(int x, int y) {
+        System.out.println(x + y);
     }
 
-    public static void printline(){
-        for(int i=0;i<3;i++){
-            System.out.println("-=-=-=-=-=-");
-            }
-    }
+
 
     public static void main(String[] args){
-        // method 함수에 대해서 배워보자
         // 함수는 class 안에 들어가야 한다.
-        // 함수는 class 안에 들어가야 한다.
-        printstar();
-        printline();
+        int a,b;
+        Scanner input = new Scanner(System.in);
+
+        a = input.nextInt();
+        b = input.nextInt();
+        input.close();
+
+        getsum(a,b);
+        
     }
 }

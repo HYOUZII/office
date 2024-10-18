ab484f9144ce9455b43a0cdbf4ea525eba71324b
lomo <cokeneon@sks1996.com>
2024-10-18 17:53:30 +0900

    1228] BMI 문제

--- hyeonyC_main.java
+++ hyeonyC_main.java
@@ -1,39 +1,23 @@
 import java.util.*;
 class Main{
-    //[Class 43] 메소드의 활용.
-    // public static int ajja(){
-
-    //     return 0;
-    // }
-
-    //[1222] 축구의 신 2
-    public static int GOD(int time, int A){
-        int result = A;
-        for(int i=0 ; i<90-time ; i+=5){
-            
-            System.out.println("goal!!");
-            result++;
-            
-        }
-        return result;
-    }
-
-    public static void main(String[] args){
-        
-        int add1,current, Class1=5, Class2=7;
+    public static void BMI(){// 비만도 계산 함수
+        float tall, weight;
         Scanner input = new Scanner(System.in);
+        float obesity, strd_obesity_level;
 
-        current = input.nextInt();
-        add1 = input.nextInt();
-        Class2 = input.nextInt();
-        input.close();
+        tall = input.nextFloat();   //입력단
+        weight = input.nextFloat();
+        input.close();              //입력해제
+        
+        strd_obesity_level = (float)((tall-100)*0.9);
+        obesity = (float)(weight-strd_obesity_level)*100/strd_obesity_level;
 
-        Class1 = GOD(current, add1);
-        System.out.println(Class1+" : "+Class2);
+        if (obesity<=10) {System.out.println("정상");} 
+        else if (obesity>20){System.out.println("비만");}
+        else{System.out.println("과체중");}
 
-        if (Class1 >= Class2) {
-            if (Class1 == Class2){System.out.println("same");}
-            else{System.out.println("win");}
-        } else {System.out.println("lose");}
+    }
+    public static void main(String[] args){
+        BMI();
     }
 }

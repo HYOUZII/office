72ae705f83c431fdeb93a443fd5192f84b20854d
lomo <cokeneon@sks1996.com>
2024-10-21 11:11:40 +0900

    for문 새로운 작성법 배움

--- hyeonyC_main.java
+++ hyeonyC_main.java
@@ -1,23 +1,17 @@
-import java.util.*;
+//for문 새로운 작성법 배움
+
 class Main{
-    public static void BMI(){// 비만도 계산 함수
-        float tall, weight;
-        Scanner input = new Scanner(System.in);
-        float obesity, strd_obesity_level;
-
-        tall = input.nextFloat();   //입력단
-        weight = input.nextFloat();
-        input.close();              //입력해제
-        
-        strd_obesity_level = (float)((tall-100)*0.9);
-        obesity = (float)(weight-strd_obesity_level)*100/strd_obesity_level;
-
-        if (obesity<=10) {System.out.println("정상");} 
-        else if (obesity>20){System.out.println("비만");}
-        else{System.out.println("과체중");}
-
+    public static int evensumorOdd(int[] nums){
+        int sum =0;
+        for(int i : nums){
+            if (i%2 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
     }
     public static void main(String[] args){
-        BMI();
+        int[] arry = {1,2,3,87,3,4,6,88,8,3,4};
+        System.out.println(evensumorOdd(arry));
     }
 }

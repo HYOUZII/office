d5cb405a52f15ada1f958c5915be55b959e04a73
lomo <cokeneon@sks1996.com>
2024-10-29 09:45:37 +0900

    1071번 0이 입력될 때까지 출력

--- hyeonyC_main.java
+++ hyeonyC_main.java
@@ -1,17 +1,20 @@
 //for문 새로운 작성법 배움
 
-class Main{
-    public static int evensumorOdd(int[] nums){
-        int sum =0;
-        for(int i : nums){
-            if (i%2 == 0) {
+class Main {
+    public static int evensumorOdd(int[] nums) {
+        int sum = 0;
+        for (int i : nums) {
+            if (i % 2 == 0) {
                 sum += i;
+            } else {
+                sum -= i;
             }
         }
         return sum;
     }
-    public static void main(String[] args){
-        int[] arry = {1,2,3,87,3,4,6,88,8,3,4};
+
+    public static void main(String[] args) {
+        int[] arry = { 1, 2, 3, 87, 3, 4, 6, 88, 8, 3, 4 };
         System.out.println(evensumorOdd(arry));
     }
 }

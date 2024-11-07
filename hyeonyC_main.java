814d875705efc27a57cb74d6717262271b2525a4
lomo <cokeneon@sks1996.com>
2024-11-07 09:34:26 +0900

    1091번] 수열 값 체크

--- hyeonyC_main.java
+++ hyeonyC_main.java
@@ -1,14 +1,24 @@
-// 단순반복문 연습
 import java.util.*;
 
 class Main{
-    public static void geometric(){
-        Scanner input = new Scanner(System.in);
-        int a = input.nextInt();
-        int r = input.nextInt();
-        int n = input.nextInt();
-    }
+
     public static void main(String[] args){
-        geometric();
+        arrow();
+    }    
+
+    public static void arrow(){
+        
+        Scanner input = new Scanner(System.in);
+        long a = input.nextLong();// 시작값
+        long m = input.nextLong();// 곱할값
+        long d = input.nextLong();// 더할 값
+        long n = input.nextLong();// 몇 번째
+        input.close();
+        long result=a;
+        for(int i=1 ; i<n ; i++){
+            result = result * m + d;
+            System.out.println(result);
+        }
+        System.out.printf("%d",result);
     }
 }

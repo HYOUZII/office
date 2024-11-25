6480aca494ec4b01e9500104c3a2a3b755efde80
lomo <cokeneon@sks1996.com>
2024-11-25 17:21:40 +0900

    instance method의 활용

--- hyeonyC_main.java
+++ hyeonyC_main.java
@@ -1,28 +1,59 @@
+class calculator {
+    int[] cnt = new int[5];
 
-class triangle{// 이 안에 변수(instance variable), 메서드(instance method)를 넣는다.
-    //instance variable
-    double base, hight;
-
-    // instance method
-    void init(double b, double h){
-        base = b;
-        hight = h;
+    double sum(double n1, double n2) {
+        cnt[0]++;
+        System.out.println(cnt[0] + "번 실행함.");
+        return n1 + n2;
     }
 
-    double getArea(){
-        return base * hight / 2;
+    double sub(double n1, double n2) {
+        cnt[1]++;
+        System.out.println(cnt[1] + "번 실행함.");
+        return n1 - n2;
+    }
+
+    double mul(double n1, double n2) {
+        cnt[2]++;
+        System.out.println(cnt[2] + "번 실행함.");
+        return n1 * n2;
+    }
+
+    double div(double n1, double n2) {
+        cnt[3]++;
+        System.out.println(cnt[3] + "번 실행함.");
+        return n1 / n2;
+    }
+
+    double power(double n1, double n2) {
+        double val = 1;
+        cnt[4]++;
+        System.out.println(cnt[4] + "번 실행함.");
+        for (int i = 0; i < n2; i++) {
+            val *= n1;
+        }
+        return val;
+    }
+
+    void printor() {
+        System.out.println("\n\n <<연산 횟수 출력 결과>> \n\n");
+        System.out.println("덧셈 : " + cnt[0] + "회");
+        System.out.println("뺄셈 : " + cnt[1] + "회");
+        System.out.println("곱셈 : " + cnt[2] + "회");
+        System.out.println("나눗셈 : " + cnt[3] + "회");
+        System.out.println("거듭제곱 : " + cnt[4] + "회");
     }
 }
-class Main{
 
-    public static void main(String[] args){
-        triangle t1 = new triangle();
-        triangle t2 = new triangle();
-        
-        t1.init(3, 5);
-        t2.init(10, 91);
+public class hyeonyC_main {
 
-        System.out.println("t1삼각형의 넓이 = "+t1.getArea());
-        System.out.println("t1삼각형의 넓이 = "+t2.getArea());
+    public static void main(String[] args) {
+        calculator C1 = new calculator();
+
+        System.out.println(C1.sub(1, 2));
+        System.out.println(C1.sub(5, 1));
+        System.out.println(C1.power(2, 2));
+
+        C1.printor();
     }
 }

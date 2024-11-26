3db83e48a79b535417645ca5e1d261479f26bc95
lomo <cokeneon@sks1996.com>
2024-11-26 13:43:41 +0900

    알파벳 대소문자 변환 완료

--- CodeUp.java
+++ CodeUp.java
@@ -1,47 +1,30 @@
 import java.util.*;
-
-class Isitright {
-    void Whatnum(int n) {
-        int a, b,flge = 0;
-        if (n > 1) {
-            for (int i = 2; i <= n; i++) {
-                if(n%i==0 & n==i){
-                    System.out.println("wrong number");
-                    break;
-                }
-                else if(n%i==0){
-                    a = i;
-                    b = n / i;
-                    for (int j = 2; j <= a; j++) {
-                        flge += (a%j==0 & a!=j) ? 1 : 0;
-                    }
-                    for (int k = 2; k <= b; k++) {
-                        flge += (b%k==0 & b!=k) ? 1 : 0;
-                    }
-                    if (flge>=1) {
-                        System.out.println("wrong number");
-                        break;
-                    }else{
-                        System.out.println(a+" "+b);
-                        break;
-                    }
-                }
-            }
-        }else {
-            System.out.println("wrong number");
+class Isitright{
+    void Whatnum(String val){
+        int len = val.length();
+        char[] arry = new char[len];
+        for (int i = 0; i < arry.length; i++) {
+            arry[i] = val.charAt(i);
+            
         }
+
+        for (int i = 0; i < arry.length; i++) {
+            if(arry[i]>='A' & arry[i]<='Z'){System.out.printf("%c",arry[i]+32);}
+            else if(arry[i]>='a' & arry[i]<='z'){System.out.printf("%c",arry[i]-32);}
+            else{System.out.printf("%c",arry[i]);}
+        }        
     }
 }
 
 class Main {
 
     public static void main(String[] args) {
-        int num;
+        String val;
         Scanner input = new Scanner(System.in);
-        num = input.nextInt();
+        val = input.nextLine();
         input.close();
 
         Isitright Q1 = new Isitright();
-        Q1.Whatnum(num);
+        Q1.Whatnum(val);
     }
 }

375281ea583bbb2a9a28b923c9f58ab9730afffc
lomo <cokeneon@sks1996.com>
2024-12-03 15:53:42 +0900

    1082번 성공

--- CodeUp.java
+++ CodeUp.java
@@ -1,34 +1,25 @@
 import java.util.*;
 
-class cipher {
-    void Caesar(String in){
-        char[] brutus = new char[in.length()];
-        for (int i = 0; i < brutus.length; i++) {
-            brutus[i] = in.charAt(i);
+class gugu{
+    void AtoF(char some){
+        int out = some-55;
+        for (int i = 1; i <= 15; i++) {
+            System.out.printf("%X*%X=%X\n",out,i,out*i); 
         }
-
-        for (int i = 0; i < brutus.length; i++) {
-            if (brutus[i]>='a' & brutus[i]<='c') {
-                System.out.printf("%c",brutus[i]+23);
-            }
-            else if(brutus[i]==32){
-                System.out.printf("%c",brutus[i]);
-            }
-            else{
-                System.out.printf("%c",brutus[i]-3);
-            }
-        }
-
-    }    
-}
-class Main {
-    public static void main(String[] args){
-        Scanner getScanner = new Scanner(System.in);
-        String input = getScanner.nextLine();
-        getScanner.close();
-
-        cipher codCipher = new cipher();
-        codCipher.Caesar(input);
     }
-    
+}
+
+class Main{
+    public static void main(String[] args){
+        char some;
+        gugu testgugu = new gugu();
+        Scanner input = new Scanner(System.in);
+        some = input.next().charAt(0);
+        input.close();
+
+        if ('A'<=some & some <= 'F') {
+            testgugu.AtoF(some);
+        }
+
+    }
 }

77225d5391f35c3e73741197ae3715146c5c9b6a
lomo <cokeneon@sks1996.com>
2025-05-08 08:57:14 +0900

    250508)아침출근전

--- CodeUp.java
+++ CodeUp.java
@@ -2,35 +2,20 @@ import java.util.*;
 
 class Main{
     public static void main(String[] args) {
-        // 먼저 입력될 데이터의 개수 n이 입력된다.
-        // 두 번째로 공백을 기준으로 n개 데이터가 입력된다.
-        // 1. 무한반복 됨
-        // 2. 입력이 끝나면 종료 메세지를 출력하면서 변수를 초기화
         Scanner input = new Scanner(System.in);
-            while (true) {
-                try {
-                    System.out.println("입력할 데이터의 수를 정수로 표현해주세요.");
-                    int Max = input.nextInt();
-                    System.out.println("입력될 데이터의 수는 \'"+Max+"\'입니다.");
-                    int[] arr = new int[Max];
-                    if (Max == 4 || Max == 0) {
-                        System.out.println("프로그램을 정지합니다.");
-                        break;
-                    }
-
-                    for (int i = 0; i < arr.length; i++) {
-                        arr[i] = input.nextInt();
-                    }
-
-                    for (int j = arr.length-1; j >= 0; j--) {
-                        System.out.print(arr[j]+" / ");
-                    }
-                    System.out.println();
-                    System.out.println("역순 출력을 마쳤습니다.\n\n\n");
-                } catch (NegativeArraySizeException e) {    
-                    System.out.println("0을 제외한 자연수를 입력해주세요.\n\n");
-                }
-                //finally{}
+        //System.out.println("입력할 수의 갯수는?");
+        int Max = input.nextInt(); // k<=100 개가 입력됨.
+        //System.out.println("갯수는 "+Max+"입니다.");
+        int[] arr = new int[Max];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = input.nextInt();
+        }
+        int token = 2;
+        while (token > 0) {
+            token--;
+            for (int j = 0; j < arr.length; j++) {
+                System.out.println(arr[j]);
+            }
         }
         input.close();
     }
